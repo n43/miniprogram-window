a1b97c8629ac7dd6e2b4659a4234d69cc8e35294
cztflove <502672047@qq.com>
2019-07-31 18:50:04 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const { miniprogramWindow } = require('./provider');
+const { miniprogramWindow } = require("./provider");
 
 Component({
   properties: {
     title: {
       type: String,
-      value: '',
-      observer: 'propChange',
+      value: "",
+      observer: "propChange"
     },
     navigationBarColor: {
       type: String,
-      value: '',
-      observer: 'propChange',
+      value: "",
+      observer: "propChange"
     },
     backgroundColor: {
       type: String,
-      value: '',
-      observer: 'propChange',
+      value: "",
+      observer: "propChange"
     },
     backgroundTextStyle: {
       type: String,
-      value: '',
-      observer: 'propChange',
+      value: "",
+      observer: "propChange"
     },
     shareContent: {
-      type: Object,
-      value: null,
-    },
+      type: null,
+      value: null
+    }
   },
   lifetimes: {
     created() {
@@ -34,27 +34,27 @@ Component({
         const attrs = this.getMountedAttrs();
         let navigationBarOptions = {};
         let backgroundColorOptions = {};
-        let backgroundTextStyle = '';
+        let backgroundTextStyle = "";
 
         if (attrs.navigationBarColor) {
           let { navigationBarColor } = attrs;
-          navigationBarColor = navigationBarColor.split(' ');
+          navigationBarColor = navigationBarColor.split(" ");
           let [backgroundColor, frontColor] = navigationBarColor;
 
           navigationBarOptions.backgroundColor = backgroundColor;
 
-          if (frontColor === '#ffffff' || frontColor === '#000000') {
+          if (frontColor === "#ffffff" || frontColor === "#000000") {
             navigationBarOptions.frontColor = frontColor;
           }
         }
 
         if (attrs.backgroundColor) {
           let { backgroundColor } = attrs;
-          backgroundColor = backgroundColor.split(' ');
+          backgroundColor = backgroundColor.split(" ");
           const [
             normalBackgroundColor,
             backgroundColorTop,
-            backgroundColorBottom,
+            backgroundColorBottom
           ] = backgroundColor;
 
           backgroundColorOptions.backgroundColor = normalBackgroundColor;
@@ -69,8 +69,8 @@ Component({
         }
 
         if (
-          attrs.backgroundTextStyle === 'dark' ||
-          attrs.backgroundTextStyle === 'light'
+          attrs.backgroundTextStyle === "dark" ||
+          attrs.backgroundTextStyle === "light"
         ) {
           backgroundTextStyle = attrs.backgroundTextStyle;
         }
@@ -79,14 +79,14 @@ Component({
         wx.setNavigationBarColor({
           ...defAttrs.navigationBarColor,
           ...navigationBarOptions,
-          animation: { duration: 0 },
+          animation: { duration: 0 }
         });
         wx.setBackgroundColor({
           ...defAttrs.backgroundColor,
-          ...backgroundColorOptions,
+          ...backgroundColorOptions
         });
         wx.setBackgroundTextStyle({
-          textStyle: backgroundTextStyle || defAttrs.backgroundTextStyle,
+          textStyle: backgroundTextStyle || defAttrs.backgroundTextStyle
         });
       };
 
@@ -104,25 +104,25 @@ Component({
           const instance = mountedList[idx];
           let completed = true;
           if (attrs.title === undefined) {
-            if (instance.data.title !== '') {
+            if (instance.data.title !== "") {
               attrs.title = instance.data.title;
             }
             completed = false;
           }
           if (attrs.navigationBarColor === undefined) {
-            if (instance.data.navigationBarColor !== '') {
+            if (instance.data.navigationBarColor !== "") {
               attrs.navigationBarColor = instance.data.navigationBarColor;
             }
             completed = false;
           }
           if (attrs.backgroundColor === undefined) {
-            if (instance.data.backgroundColor !== '') {
+            if (instance.data.backgroundColor !== "") {
               attrs.backgroundColor = instance.data.backgroundColor;
             }
             completed = false;
           }
           if (attrs.backgroundTextStyle === undefined) {
-            if (instance.data.backgroundTextStyle !== '') {
+            if (instance.data.backgroundTextStyle !== "") {
               attrs.backgroundTextStyle = instance.data.backgroundTextStyle;
             }
             completed = false;
@@ -177,13 +177,13 @@ Component({
       }
 
       this.componentReady = false;
-    },
+    }
   },
   methods: {
     propChange() {
       if (this.componentReady) {
         this.emitChange();
       }
-    },
-  },
+    }
+  }
 });
